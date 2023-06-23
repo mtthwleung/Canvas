549a88491a3543dab6bf3ff02e3479497880e406
claraslamm <claraslam@outlook.com>
2023-06-23 11:58:48 +0800

    added pen width, fill color functionality
    things need to do:
    
    color btn and color picker isnt working yet
    
    eraser needs to set color as white

--- paint.js
+++ paint.js
@@ -1,19 +1,34 @@
 const canvas = document.querySelector("canvas");
 tools = document.querySelectorAll(".tool");
 ctx = canvas.getContext("2d");
+let saveImg = document.querySelector("#save-img");
+let clearButton = document.querySelector(".clear-canvas");
+fillColor = document.querySelector("#fill-color"); 
+sizeSlider = document.querySelector("#size-slider"); 
+colorBtns = document.querySelectorAll(".colors .option");
+colorPicker = document.querySelector("#color-picker");
 
 let isDrawing = false;
 let selectedTool = "brush";
 let MouseX; // variable declared, we will use later to return the x-coordinate of our mouse when a mouse event occurs
 let MouseY; // variable declared, we will use later to return the y-coordinate of our mouse when a mouse event occurs
 let brushWidth = 5;
+let selectedColor = "#000";
 const undoStack = [];
 const redoStack = [];
+
+const whiteBackground = () => {
+    ctx.fillStyle = "white";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    ctx.fillStyle = selectedColor; // * clara
+}
+
 
 //This is used to ensure the canvas element scales correctly. Without it, sometimes you create lines/shapes on a different spot from where you clicked.
 window.addEventListener("load", () => {
     canvas.width = canvas.offsetWidth;
     canvas.height = canvas.offsetHeight;
+    whiteBackground();
 })
 
 //When we click each tool button, the below happens...
@@ -23,8 +38,8 @@ tools.forEach(btn => {
         btn.classList.add("active") // adds 'active' class to the button that you click
         selectedTool = btn.id; // the id of the button you click becomes selectedTool
         console.log(selectedTool); // just to see which tool we have chosen on the console
-    })
-})
+    });
+});
 
 //When "mousedown" occurs, this function executes:
 const startDrawing = (e) => {
@@ -33,6 +48,8 @@ const startDrawing = (e) => {
     MouseY = e.offsetY; // setting these 2 variables to become the coordinates of our mouse when "mousedown" event fires
     ctx.beginPath();
     ctx.lineWidth = brushWidth;
+    ctx.strokeStyle = selectedColor;
+    ctx.fillStyle = selectedColor;
     snapshot = ctx.getImageData(0, 0, canvas.width, canvas.height); // copying canvas data to avoid dragging for rectangles
 }
 
@@ -40,7 +57,9 @@ const startDrawing = (e) => {
 const drawing = (e) => {
     if (!isDrawing) return; // below code will not run if isDrawing === false
     ctx.putImageData(snapshot, 0, 0);
-    if (selectedTool === "brush") {
+
+    if (selectedTool === "brush" || selectedTool === "eraser") {
+        ctx.strokeStlye = selectedTool === "eraser" ? "#fff" : selectedColor;
         ctx.lineTo(e.offsetX, e.offsetY);
         ctx.stroke();
     } else if (selectedTool === "rectangle") {
@@ -49,6 +68,10 @@ const drawing = (e) => {
         drawLine(e);
     } else if (selectedTool === "curvedLine") {
         drawCurvedLine(e);
+    } else if (selectedTool === "triangle"){   
+        drawTriangle(e);
+    } else if (selectedTool === "circle"){
+        drawCricle(e)
     }
 }
 
@@ -59,21 +82,64 @@ const stopDrawing = () => {
 }
 
 const drawRect = (e) => {
-    ctx.strokeRect(e.offsetX, e.offsetY, MouseX - e.offsetX, MouseY - e.offsetY);
+    if(!fillColor.checked) {
+    return ctx.strokeRect(e.offsetX, e.offsetY, MouseX - e.offsetX, MouseY - e.offsetY);
+    }   
+    ctx.fillRect(e.offsetX, e.offsetY, MouseX - e.offsetX, MouseY - e.offsetY);
+}
+
+const drawTriangle = (e) => {         //drawing triangle//
+    ctx.beginPath();
+    ctx.moveTo(MouseX, MouseY);
+    ctx.lineTo(e.offsetX, e.offsetY);
+    ctx.lineTo(MouseX*2 - e.offsetX, e.offsetY)
+    ctx.closePath()
+    ctx.stroke()
+    fillColor.checked ? ctx.fill() : ctx.stroke(); // if fillColor is checked fill triangle
+}
+
+const drawCricle = (e) => {
+    //formula to calculate distance between initial and final cursor coordinates to calculate radius//
+    let radius = Math.sqrt(Math.pow((MouseX - e.offsetX),2) + Math.pow(MouseY - e.offsetY,2)); 
+     ctx.beginPath()
+    ctx.arc(MouseX,MouseY, radius, 0, 2*Math.PI );  //the calculated radius is put here in the parameters//
+    fillColor.checked ? ctx.fill() : ctx.stroke();
 }
 
 const drawLine = (e) => {
+    let brush = "black";
+    let brushWidth = 5;
+
+    ctx.beginPath()
+    ctx.moveTo(MouseX,MouseY);
+    ctx.lineTo(e.offsetX,e.offsetY);
+
+    ctx.moveTo(MouseX, MouseY);
+    ctx.lineTo(e.offsetX, e.offsetY);
+    ctx.stroke();
 
 }
 
 const drawCurvedLine = (e) => {
 
+    let brush = "black";
+    let brushWidth = 5;
+
+    ctx.beginPath();
+    ctx.moveTo(MouseX,MouseY);
+    ctx.bezierCurveTo(e.offsetX*2,e.offsetY/2,e.offsetX*2,e.offsetY/2,e.offsetX+100,e.offsetY+100);
+
+
+
+    ctx.stroke();
 }
+
 
 const undo = () => {
     redoStack.push(undoStack.pop()); // removes last snapshot from undo stack
     ctx.clearRect(0, 0, canvas.width, canvas.height) // clears the canvas
     undoStack.forEach(snapshot => {
+        console.log("put imagedata");
         ctx.putImageData(snapshot, 0, 0);
     })
 }
@@ -95,3 +161,37 @@ canvas.addEventListener("mouseup", stopDrawing);
 
 document.querySelector("#undo").addEventListener("click", undo);
 document.querySelector("#redo").addEventListener("click", redo);
+
+document.querySelector("#redo").addEventListener("click", redo);
+
+saveImg.addEventListener("click", (e) => {
+  const link = document.createElement("a");
+  link.download = "My awesome drawing.jpg";
+  link.href = canvas.toDataURL("image/jpg");
+  link.click();
+});
+
+clearButton.addEventListener("click", () => {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    whiteBackground();
+});
+
+sizeSlider.addEventListener("change", () => brushWidth = sizeSlider.value);
+
+
+//* trying to get the below function work *// 
+
+colorBtns.forEach(btn => {
+    btn.addEventListener("click", () => { // adding click event to all color button
+      document.querySelector(".option .selected").classList.remove("selected");
+      btn.classList.add("selected");
+      // passing selected btn background color as selectedColor value
+      selectedColor = window.getComputedStyle(btn).getPropertyValue("background-color");
+    });
+  });
+  
+  colorPicker.addEventListener ("change", () => {
+    // passing picked color value from color picker to last color btn background
+    colorPicker.parentElement.style.background = colorPicker.value;
+    colorPicker.parentElement.click();
+  })
